ef2c55de4c06a2c3bfb2b839520c443c6bc8a2c1
Gabriel Sobral <gabriel.sobral1367@gmail.com>
2025-12-26 15:57:03 -0300

    fix appearance dialog theme switcher responsivity

--- next-app/src/app/dashboard/[organizationId]/application/(components)/application-card.tsx
+++ next-app/src/app/dashboard/[organizationId]/application/(components)/application-card.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { AlertCircle } from "lucide-react";
 import { useParams } from "next/navigation";
 
 import {
@@ -11,12 +12,11 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-
-import { useApplicationsSWR } from "@/services/dashboard/use-applications-swr";
 import { Skeleton } from "@/components/ui/skeleton";
-import { APIError } from "@/types/service-options";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { AlertCircle } from "lucide-react";
+
+import { APIError } from "@/types/service-options";
+import { useApplicationsSWR } from "@/services/dashboard/use-applications-swr";
 
 export function ApplicationCard() {
   const organizationId = useParams().organizationId as string;
@@ -63,21 +63,25 @@ export function ApplicationCard() {
         key={application.id}
         href={`/dashboard/${organizationId}/application/${application.id}`}
       >
-        <Card className="w-[calc(33.333%-8px)] min-w-[400px] transition-all hover:scale-[1.01] hover:cursor-pointer hover:shadow-lg">
-          <CardHeader>
-            <CardTitle>{application.name}</CardTitle>
-            <CardDescription className="line-clamp-4">
-              {application.description}
-            </CardDescription>
-          </CardHeader>
+        <Card className="w-[calc(33.333%-8px)] min-w-[400px] transition-all hover:scale-[1.01] hover:cursor-pointer hover:shadow-lg overflow-hidden flex gap-3">
+          <div className="bg-primary min-w-[16px] min-h-[133px]"></div>
+          
+          <div>
+            <CardHeader>
+              <CardTitle>{application.name}</CardTitle>
+              <CardDescription className="line-clamp-4">
+                {application.description}
+              </CardDescription>
+            </CardHeader>
 
-          <CardFooter className="mt-3">
-            {application.badges.map((badge, i) => (
-              <Badge key={i} variant="outline">
-                {badge}
-              </Badge>
-            ))}
-          </CardFooter>
+            <CardFooter className="mt-3">
+              {application.badges.map((badge, i) => (
+                <Badge key={i} variant="outline">
+                  {badge}
+                </Badge>
+              ))}
+            </CardFooter>
+          </div>
         </Card>
       </Link>
     )) || []
--- next-app/src/components/settings-dialog/appearance-section/theme-switcher.tsx
+++ next-app/src/components/settings-dialog/appearance-section/theme-switcher.tsx
@@ -12,9 +12,9 @@ export function ThemeSwitcher() {
     <RadioGroup
       value={theme}
       onValueChange={(val) => setTheme(val)}
-      className="flex gap-4 mt-4"
+      className="flex gap-4 mt-4  w-full relative"
     >
-      <div className="relative flex flex-col gap-4">
+      <div className="relative flex flex-col gap-4 w-full max-w-[33.33%]">
         <RadioGroupItem
           id="theme-light"
           value="light"
@@ -23,51 +23,51 @@ export function ThemeSwitcher() {
 
         <Label
           htmlFor="theme-light"
-          className="cursor-pointer rounded-lg border shadow w-[330px] h-[190px] p-2 flex gap-3 transition-all peer-data-[state=checked]:border-primary peer-data-[state=checked]:ring-6 peer-data-[state=checked]:ring-primary/30 bg-white"
+          className="cursor-pointer rounded-lg border shadow w-full h-[190px] p-2 flex gap-3 transition-all peer-data-[state=checked]:border-primary peer-data-[state=checked]:ring-6 peer-data-[state=checked]:ring-primary/30 bg-white"
         >
-          <div className="h-full bg-gray-200 w-[60px] rounded-md" />
+          <div className="h-full bg-gray-200 w-[20%] rounded-md" />
           <div className="w-full flex flex-col gap-2 ">
-            <div className="h-3 w-[90px] bg-gray-200 rounded" />
-            <div className="h-3 w-[190px] bg-gray-200 rounded" />
+            <div className="h-3 w-[33%] bg-gray-200 rounded" />
+            <div className="h-3 w-[80%] bg-gray-200 rounded" />
             <div className="h-8 w-full bg-gray-200 rounded" />
-            <div className="flex gap-2">
-              <div className="h-[30px] w-[70px] bg-gray-200 rounded" />
-              <div className="h-[30px] w-[70px] bg-gray-200 rounded" />
-              <div className="h-[30px] w-[70px] bg-gray-200 rounded" />
+            <div className="flex gap-2 relative h-[30px]">
+              <div className="h-full w-[70%] bg-gray-200 rounded" />
+              <div className="h-full w-[70%] bg-gray-200 rounded" />
+              <div className="h-full w-[70%] bg-gray-200 rounded" />
             </div>
-            <div className="h-3 w-[150px] bg-gray-200 rounded" />
-            <div className="h-3 w-[226px] bg-gray-200 rounded" />
+            <div className="h-3 w-[60%] bg-gray-200 rounded" />
+            <div className="h-3 w-[90%] bg-gray-200 rounded" />
           </div>
         </Label>
 
         <span className="text-lg font-semibold">Light</span>
       </div>
 
-      <div className="relative flex flex-col gap-4">
+      <div className="relative flex flex-col gap-4 w-full max-w-[33.33%]">
         <RadioGroupItem id="theme-dark" value="dark" className="peer sr-only" />
         <Label
           htmlFor="theme-dark"
-          className="cursor-pointer rounded-lg border border-gray-500 shadow w-[330px] h-[190px] p-3 flex gap-2 bg-gray-900 transition-all peer-data-[state=checked]:border-primary peer-data-[state=checked]:ring-6 peer-data-[state=checked]:ring-primary/30"
+          className="cursor-pointer rounded-lg border border-gray-500 shadow w-full h-[190px] p-3 flex gap-2 bg-gray-900 transition-all peer-data-[state=checked]:border-primary peer-data-[state=checked]:ring-6 peer-data-[state=checked]:ring-primary/30"
         >
-          <div className="h-full bg-gray-700 w-[60px] rounded-md" />
+          <div className="h-full bg-gray-700 w-[20%] rounded-md" />
           <div className="w-full flex flex-col gap-2">
-            <div className="h-3 w-[90px] bg-gray-700 rounded" />
-            <div className="h-3 w-[190px] bg-gray-700 rounded" />
+            <div className="h-3 w-[33%] bg-gray-700 rounded" />
+            <div className="h-3 w-[80%] bg-gray-700 rounded" />
             <div className="h-8 w-full bg-gray-700 rounded" />
-            <div className="flex gap-2">
-              <div className="h-[30px] w-[70px] bg-gray-700 rounded" />
-              <div className="h-[30px] w-[70px] bg-gray-700 rounded" />
-              <div className="h-[30px] w-[70px] bg-gray-700 rounded" />
+            <div className="flex gap-2 relative h-[30px]">
+              <div className="h-full w-[70%] bg-gray-700 rounded" />
+              <div className="h-full w-[70%] bg-gray-700 rounded" />
+              <div className="h-full w-[70%] bg-gray-700 rounded" />
             </div>
-            <div className="h-3 w-[150px] bg-gray-700 rounded" />
-            <div className="h-3 w-[226px] bg-gray-700 rounded" />
+            <div className="h-3 w-[60%] bg-gray-700 rounded" />
+            <div className="h-3 w-[90%] bg-gray-700 rounded" />
           </div>
         </Label>
 
         <span className="text-lg font-semibold">Dark</span>
       </div>
 
-      <div className="relative flex flex-col gap-4">
+      <div className="relative flex flex-col gap-4 w-full max-w-[33.33%]">
         <RadioGroupItem
           id="theme-system"
           value="system"
@@ -75,23 +75,23 @@ export function ThemeSwitcher() {
         />
         <Label
           htmlFor="theme-system"
-          className="relative cursor-pointer rounded-lg  shadow-lg w-[330px] h-[190px] p-3 flex gap-2 bg-black transition-all peer-data-[state=checked]:border-primary peer-data-[state=checked]:ring-6 peer-data-[state=checked]:ring-primary/30"
+          className="relative cursor-pointer rounded-lg  shadow-lg w-full h-[190px] p-3 flex gap-2 bg-black transition-all peer-data-[state=checked]:border-primary peer-data-[state=checked]:ring-6 peer-data-[state=checked]:ring-primary/30"
         >
-          <div className="h-full bg-gray-800 w-[60px] rounded-md" />
+          <div className="h-full bg-gray-800 w-[20%] rounded-md" />
           <div className="w-full flex flex-col gap-2">
-            <div className="h-3 w-[90px] bg-gray-800 rounded" />
-            <div className="h-3 w-[190px] bg-gray-800 rounded" />
+            <div className="h-3 w-[33%] bg-gray-800 rounded" />
+            <div className="h-3 w-[80%] bg-gray-800 rounded" />
             <div className="h-8 w-full bg-gray-800 rounded" />
-            <div className="flex gap-2">
-              <div className="h-[30px] w-[70px] bg-gray-800 rounded" />
-              <div className="h-[30px] w-[70px] bg-gray-800 rounded" />
-              <div className="h-[30px] w-[70px] bg-gray-800 rounded" />
+            <div className="flex gap-2 relative h-[30px]">
+              <div className="h-full w-[70%] bg-gray-800 rounded" />
+              <div className="h-full w-[70%] bg-gray-800 rounded" />
+              <div className="h-full w-[70%] bg-gray-800 rounded" />
             </div>
-            <div className="h-3 w-[150px] bg-gray-800 rounded" />
-            <div className="h-3 w-[226px] bg-gray-800 rounded" />
+            <div className="h-3 w-[60%] bg-gray-800 rounded" />
+            <div className="h-3 w-[90%] bg-gray-800 rounded" />
           </div>
 
-          <div className="absolute w-[176px] h-[190px] top-0 left-0 rounded-l-lg backdrop-invert w-50%"></div>
+          <div className="absolute w-[50%] h-full top-0 left-0 rounded-l-lg backdrop-invert w-50%"></div>
         </Label>
 
         <span className="text-lg font-semibold">System</span>
